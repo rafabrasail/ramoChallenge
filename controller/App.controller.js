c894cad59cb7bb6ceb77ae10f50b1a7b25653075
rafabrasail <rafaelrosenberg89@gmail.com>
2023-05-10 10:44:05 -0300

    update on Detail view

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -11,23 +11,6 @@ sap.ui.define([
    return Controller.extend("sap.ui.demo.controller.App", {
       onInit: function () {
 
-         var that = this
-         jQuery.ajax({
-            method: "GET",
-            url: "https://jsonplaceholder.typicode.com/todos",
-            success: function(data){
-
-               // 1) define model
-               var oModel = new sap.ui.model.json.JSONModel(data);
-
-               // 2) model set to to View
-               that.getView().setModel(oModel, "todosData");
-               //console.log(data)
-            }, 
-            error: function(error){
-               console.log(error)
-            }
-         })
       },
       mySerach : function(oEvent) {
          // get the search query entered by the user
@@ -42,17 +25,12 @@ sap.ui.define([
          oBinding.filter([oFilter]);
       },
       onPress : function(oEvent) {
-         alert("Navigating to Detil of " + oEvent.getSource().getBindingContext());
-         // var oItem = oEvent.getSource()
-         // var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			// oRouter.navTo("detail", {
-         //    detailPath: oItem.getBindingContext("todosData").getProperty("id")
-         //    // detailPath: window.encodeURIComponent(oItem.getBindingContext("detail").getPath().substr(1))
-         // });
-      },
-      onToPage2 : function() {
-         this.getOwnerComponent().getRouter().navTo("detail");
+         var oItem = oEvent.getSource()
+         var oRouter = this.getOwnerComponent().getRouter()
+			oRouter.navTo("detail", {
+            detailPath: window.encodeURIComponent(oItem.getBindingContext("todosModel").getPath().substr(1)),
+         })
+         // console.log(window.encodeURIComponent(oItem.getBindingContext("todosModel").getPath().substr(1)))
       }
-
    })
 })
--- controller/Detail.controller.js
+++ controller/Detail.controller.js
@@ -1,15 +1,49 @@
 sap.ui.define( [
     "sap/ui/core/mvc/Controller", 
-    "sap/ui/core/routing/History"], function (Controller, History) {
+    "sap/ui/core/routing/History",
+	"sap/ui/model/json/JSONModel"], function (Controller,
+	History,
+	JSONModel) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.controller.Detail", {
 		onInit : function () {
-			// var sUrl = "#" + this.getOwnerComponent().getRouter().getURL("page1");
-			// this.byId("link").setHref(sUrl);
+			var oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("detail").attachPatternMatched(this._onDetailMatched, this);
+
+		},
+		_onDetailMatched: function (oEvent) {
+			var sDetailPath = oEvent.getParameter("arguments").detailPath;
+			console.log("detail", sDetailPath)
+
+			this._getMoreInfo(sDetailPath)
 		},
 		onBack : function () {
-			this.getOwnerComponent().getRouter().navTo("overview");
+            var oHistory = History.getInstance()
+            var sPreviousHash = oHistory.getPreviousHash()
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1)
+            } else {
+                var oRouter = this.getOwnerComponent().getRouter()
+                oRouter.navTo("overview", {},true)
+            }
+		},
+		_getMoreInfo: function (sDetailPath) {
+			var that = this
+			jQuery.ajax({
+				method: "GET",
+				url: "https://jsonplaceholder.typicode.com/todos/"+sDetailPath,
+				success: function(data){
+					// 1) define model from selecte element
+					var oModel = new sap.ui.model.json.JSONModel(data);
+					// 2) set model on the parent View
+					 that.getView().setModel(oModel, "todosDetail");
+					 that.getView().getParent().setModel(oModel, "todosDetail");
+				},
+				error: function(error){
+					console.log(error)
+				}
+			})
 		}
 	});
-
 });
